f78c09e1157f26f0c80d72b40b6ddbc9ee1a1c05
amacleod33 <alexander.macleod.18@cnu.edu>
2020-02-02 14:21:27 -0500

    first commit

--- src/Pig.java
+++ src/Pig.java
@@ -5,34 +5,41 @@ import java.util.Scanner;
  * Pig plays the game Pig as outlined by the rules on Wikipedia:
  * https://en.wikipedia.org/wiki/Pig_(dice_game)
  * 
- * @author CPSC255 class
+ * @author Alex MacLeod
  * @version 1.0
  */
 public class Pig {
 
-    // ------------------------------------------------------
-    // @TODO - fix this by defining the required fields here (see API)
-    // You MUST use the same names and modifiers
-
+    /** YES is Constant used to define proper "Yes" answer */
+    static final String YES = "Y";
+    /** NO is Constant used to define proper "No" answer */
+    static final String NO  = "N";
+    /** randGen is instance of java random number generator */
+    static Random       randGen;
+    /** maxPoints is a static variable that can be used the whole program */
+    static int          maxPoints;
     // -------------------------------
     // Method definitions go below
 
     /**
-     * getRandom returns a random number between start and end (or between end and
-     * start if end {@literal <} start) based on the Random object randGen,
+     * getRandom returns a random number between start and end (or between end
+     * and start if end {@literal <} start) based on the Random object randGen,
      * inclusive of both end points.
      * 
      * @param start
-     *                  the lowest possible number
+     *            the lowest possible number
      * @param end
-     *                  the highest possible number
+     *            the highest possible number
      * @return an integer between (and including) start and end
      */
     public static int getRandom(int start, int end) {
-
-        return Integer.MIN_VALUE; // @todo - fix this method
+        if (end < start) {
+            return randGen.nextInt(start - end + 1) + end;
+        }
+        return randGen.nextInt(end - start + 1) + start;
 
     }
+
 
     /**
      * Returns a number between 1 and 6. Calls getRandom
@@ -40,66 +47,114 @@ public class Pig {
      * @return a number between 1 and 6
      */
     public static int roll() {
-
-        return Integer.MIN_VALUE; // @todo - fix this method
+        return getRandom(1, 6);
 
     }
 
+
     /**
-     * Returns "Y" or "N" based on what the user enters If the user enters y or Y as
-     * the first character, getYNAnswer returns Pig.YES. Otherwise, it returns
-     * Pig.NO
+     * Returns "Y" or "N" based on what the user enters If the user enters y or
+     * Y as the first character, getYNAnswer returns Pig.YES. Otherwise, it
+     * returns Pig.NO
      * 
      * @param kbd
-     *                   reference of single scanner object created to handle
-     *                   keyboard input
+     *            reference of single scanner object created to handle keyboard
+     *            input
      * @param prompt
-     *                   user prompt string that should be printed
+     *            user prompt string that should be printed
      * @return Y or N based on user input
      */
     public static String getYNAnswer(Scanner kbd, String prompt) {
-
-        return "FIX THIS METHOD";
+        System.out.println(prompt);
+        String finalAns = kbd.next();
+        System.out.print("First Letter: " + finalAns.substring(0));
+        if (Character.toUpperCase(finalAns.charAt(0)) == 'Y') {
+            return Pig.YES;
+        }
+        return Pig.NO;
 
     }
 
+
     /**
      * This method allows the user to have a turn at Pig. The user rolls, and
-     * continues to roll and accumulate points until the user rolls a 1 (in which
-     * case the player receive a 0 for the turn), or until the user decides to stop
-     * (in which case the method return the total of each dice roll in this turn).
+     * continues to roll and accumulate points until the user rolls a 1 (in
+     * which case the player receive a 0 for the turn), or until the user
+     * decides to stop (in which case the method return the total of each dice
+     * roll in this turn).
      * <p>
      * This method should call getYNAnswer and roll. Uses constants Pig.YES and
      * Pig.NO. Do not use "magic" values like "y".
      * 
      * @param kbd
-     *                the Scanner method. It is important NOT to create a new
-     *                Scanner object in any method. If you do, your junit tests will
-     *                fail.
+     *            the Scanner method. It is important NOT to create a new
+     *            Scanner object in any method. If you do, your junit tests will
+     *            fail.
      * @return the total points for this turn
      */
     public static int takeTurn(Scanner kbd) {
+        System.out.println("Player's turn");
+        System.out.println("-------------");
+        int totalPoints = 0;
+        String answer = "";
 
-        return Integer.MIN_VALUE; // @todo - fix this method
+        while (totalPoints < maxPoints) {
+            answer = getYNAnswer(kbd, "Roll? Y/N");
+            if (answer.equals(Pig.YES)) {
+                int roll = roll();
+                if (roll == 1) {
+                    System.out.println("Oh no! You rolled a 1!");
+                    totalPoints = 0;
+                    return totalPoints;
+                }
+                else {
+                    totalPoints += roll;
+                }
+                System.out.println("ROLL: " + roll);
+                System.out.println("Points: " + totalPoints);
 
+            }
+            else if (answer.equals(Pig.NO)) {
+                return totalPoints;
+            }
+
+        }
+        return totalPoints;
     }
 
+
     /**
-     * This simulates a computer turn. The computer rolls a random number of times
-     * between 1 and maxComputerRolls. If the computer rolls a 1, the turn is over,
-     * and the computer receives a 0. Otherwise, the computer continues to roll the
-     * random number of times chosen.
+     * This simulates a computer turn. The computer rolls a random number of
+     * times between 1 and maxComputerRolls. If the computer rolls a 1, the turn
+     * is over, and the computer receives a 0. Otherwise, the computer continues
+     * to roll the random number of times chosen.
      * 
      * @param maxComputerRolls
-     *                             the maximum number of times that the computer can
-     *                             have in a single turn
+     *            the maximum number of times that the computer can have in a
+     *            single turn
      * @return the number of points that the computer has won on this turn
      */
     public static int takeComputerTurn(int maxComputerRolls) {
+        System.out.println("Computer's turn");
+        System.out.println("----------------");
+        int totalPoints = 0;
+        for (int i = 0; i < maxComputerRolls; ++i) {
+            int roll = roll();
+            if (roll == 1) {
+                System.out.println("ROLLED A 1. 0 POINTS.");
+                totalPoints = 0;
 
-        return Integer.MIN_VALUE; // @todo - fix this method
+                return totalPoints;
+            }
+            else {
+                System.out.println("Rolled a " + roll);
+                totalPoints += roll;
+            }
+        }
+        return totalPoints;
 
     }
+
 
     /**
      * This method asks:
@@ -109,20 +164,43 @@ public class Pig {
      * </ol>
      * Then, play starts and continues (the user goes first). This method should
      * call takeTurn then takeComputerTurn. After each turn, this method should
-     * print a message indicating the number of points that has just been accrued.
-     * After one player (user or computer) reaches or exceeds the number of points
-     * needed to win, this method prints a "You win" or "you lose" message, as
-     * appropriate. You can choose the wording of the message, but it must contain
-     * "win" if the user won, or "lose" is the user lost.
+     * print a message indicating the number of points that has just been
+     * accrued. After one player (user or computer) reaches or exceeds the
+     * number of points needed to win, this method prints a "You win" or "you
+     * lose" message, as appropriate. You can choose the wording of the message,
+     * but it must contain "win" if the user won, or "lose" is the user lost.
      * 
      * @param kbd
-     *                Scanner where input is read. Pass this object as a parameter
-     *                to any methods that have input.
+     *            Scanner where input is read. Pass this object as a parameter
+     *            to any methods that have input.
      */
     public static void playGame(Scanner kbd) {
+        int playerPoints = 0;
+        int computerPoints = 0;
+        int computerRolls;
+        System.out.println("How many points will win? -> ");
+        maxPoints = kbd.nextInt();
+        System.out.println("How many times can the computer roll?");
+        computerRolls = kbd.nextInt();
+        while (playerPoints <= maxPoints && computerPoints <= maxPoints) {
 
-        // @todo - fix this method
+            playerPoints += takeTurn(kbd);
+            System.out.println();
+            if (playerPoints >= maxPoints) {
+                System.out.println("You win!");
+                break;
+            }
+            System.out.println("Player Points: " + playerPoints);
+            computerPoints += takeComputerTurn(computerRolls);
+            System.out.println();
+            System.out.println("Computer Points: " + computerPoints);
+            if (computerPoints >= maxPoints) {
+                System.out.println("You lose :=(");
+            }
+
+        }
     }
+
 
     /**
      * <ol>
@@ -136,13 +214,13 @@ public class Pig {
      * </ol>
      * 
      * @param args
-     *                 not used
+     *            not used
      */
     public static void main(String[] args) {
         Scanner kbd = new Scanner(System.in);
         randGen = new Random(); // randGen should be a static field!
         playGame(kbd);
-        String answer = getYNAnswer(kbd, "Do you want to play again?");
+        String answer = getYNAnswer(kbd, "Do you want to play again? Y/N");
         while (answer.contentEquals(YES)) { // YES is static CONSTANT field
             playGame(kbd);
             answer = getYNAnswer(kbd, "\n\nDo you want to play again?");
